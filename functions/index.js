21c7afb5d027ec11d255c8d69d0697104038e9d7
Samanja Cartagena <70960407+SamanjaCartagena@users.noreply.github.com>
2021-07-02 01:14:40 -0400

    authentication middleware added

--- functions/index.js
+++ functions/index.js
@@ -24,7 +24,6 @@ const db= admin.firestore();
 
 app.get('/screams',(req,res)=>{
     db
-     .firestore()
      .collection('screams')
      .orderBy('createdAt','desc')
      .get()
@@ -43,26 +42,51 @@ app.get('/screams',(req,res)=>{
      .catch((err)=>console.error(err));
 })
 
+const FBAuth = (req,res,next) => {
+    let idToken;
+ if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+  idToken= req.headers.authorization.split('Bearer ')[1];
+ }else{
+     console.error('No token found')
+     return res.status(403).json({error:'Unauthorized'});
+ }
+ admin.auth().verifyIdToken(idToken)
+       .then(decodedToken => {
+           req.user= decodedToken;
+           console.log(decodedToken);
+           return db.collection('users')
+           .where('userId','==',req.user.uid)
+           .limit(1)
+           .get();
+       })
+       .then(data =>{
+           req.user.handle= data.docs[0].data().handle;
+           return next();
 
+       })
+       .catch(err =>{
+           console.error('Error while verifying token',err);
+           return res.status(403).json(err);
+       })
+}
 
 
  //Post a scream
-app.post('/scream',(req,res)=>{
+app.post('/scream', FBAuth,(req,res)=>{
     if(req.body.body.trim()===''){
         return res.status(400).json({body:'Body must not be empty'});
     }
 
         const newScream = {
-            body: req.body.body,
-            userHandle: req.body.userHandle,
+            body:req.body.body,
+            userHandle: req.user.handle,
             createdAt: new Date().toISOString()
         };
         db
-        .firestore()
         .collection('screams')
         .add(newScream)
         .then((doc) =>{
-            res.json({message:`document ${doc. id} created successfully`});
+            res.json({message:`document ${doc.id} created successfully`});
         })
         .catch(err=>{
             res.status(500).json({error:' something went wrong'});
@@ -97,8 +121,8 @@ app.post('/signup',(req,res)=>{
         
     }
     if(isEmpty(newUser.password)) errors.password='Must not be empty';
-    if(newUser.password !== newUser.confirmPassword)errors.confirmPassword='Passwords must match';
-    if(isEmpty(newUser.handle))errors.handle='Must not be empty';
+    if(newUser.password !== newUser.confirmPassword) errors.confirmPassword='Passwords must match';
+    if(isEmpty(newUser.handle)) errors.handle='Must not be empty';
 
     if(Object.keys(errors).length > 0) return res.status(400).json(errors);
     let token, userId;
